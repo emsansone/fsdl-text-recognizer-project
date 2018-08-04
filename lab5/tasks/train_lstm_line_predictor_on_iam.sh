576c308b54205104f6b86b480727c1973f64d376
esansone <esansone@local>
2018-08-04 23:07:24 +0000

    Added different optimizers

--- lab5/tasks/train_lstm_line_predictor_on_iam.sh
+++ lab5/tasks/train_lstm_line_predictor_on_iam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args": {"username": "sergeyk"}}'
+pipenv run python training/run_experiment.py --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc", "train_args": {"username": "emsansone"}}'
